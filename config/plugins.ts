489804f7d74ae158d4cee1b84e6934e0dc93a192
necheporenko <ne4eporenko.v@gmail.com>
2025-05-12 12:56:37 +0300

    feat: add a strapi-algolia

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,18 @@
-export default () => ({
-  // slugify: {
-  //   enabled: true,
-  //   config: {},
-  // },
+export default ({ env }) => ({
+  "strapi-algolia": {
+    enabled: true,
+    config: {
+      apiKey: env("ALGOLIA_ADMIN_KEY"),
+      applicationId: env("ALGOLIA_APP_ID"),
+      contentTypes: [
+        {
+          name: "api::product.product",
+          populate: { Metadata: "*", Categorization: "*" },
+          hideFields: ["GalleryImages", "Recipes"],
+        },
+      ],
+    },
+  },
   graphql: {
     config: {
       endpoint: "/graphql",
