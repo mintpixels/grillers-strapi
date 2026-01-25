28646812c5d6431378dbe788d1491b6506cf66a9
mintpixels <cbollman@roundpixelstudio.com>
2026-01-25 15:01:34 -0500

    Fix cold-chain-setting folder naming to match singularName
    Properly renamed cold-chain-settings (plural) to cold-chain-setting (singular)
    using git mv to ensure Git tracks the rename correctly.
    
    Changes:
    - Renamed src/api/cold-chain-settings/ to src/api/cold-chain-setting/
    - Updated API UIDs from hybrid 'api::cold-chain-settings.cold-chain-setting'
      to correct 'api::cold-chain-setting.cold-chain-setting'
    - Updated file header comments from plural to singular
    - Removed @ts-expect-error comments now that types are correct
    
    This fixes the deployment error:
    "The key of the content-type should be the same as its singularName.
    Found cold-chain-settings and cold-chain-setting."
    
    Folder name now correctly matches singularName in schema.

---
+++ src/api/cold-chain-setting/controllers/cold-chain-setting.ts
@@ -0,0 +1,11 @@
+/**
+ * cold-chain-setting controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::cold-chain-setting.cold-chain-setting');
+
+
+
+
---
+++ src/api/cold-chain-setting/routes/cold-chain-setting.ts
@@ -0,0 +1,11 @@
+/**
+ * cold-chain-setting router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::cold-chain-setting.cold-chain-setting');
+
+
+
+
