9b940270c637ed6face96acd9b2a5ca56bc60eac
mintpixels <cbollman@roundpixelstudio.com>
2026-01-25 13:31:10 -0500

    fix

---
+++ src/api/cold-chain-setting/services/cold-chain-setting.ts
@@ -0,0 +1,11 @@
+/**
+ * cold-chain-setting service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::cold-chain-setting.cold-chain-setting');
+
+
+
+
